c57974f73dba946a18b943f0aa816ed2df73f715
Lukas Rieger <lr@live.de>
2018-03-08 23:56:40 +0100

    reproduce

---
+++ Weavers/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weavers
+{
+    public class NamedWeaver
+    {
+        public ModuleDefinition ModuleDefinition { get; set; }
+
+        public Action<string> LogWarning { get; set; }
+
+        public void Execute()
+        {
+            LogWarning("HELLO from 'NamedWeaver'");
+            //ModuleDefinition.Types.Add(new TypeDefinition("MyNamespace", "MyType", TypeAttributes.Public, ModuleDefinition.Import(typeof(object))));
+        }
+    }
+}
